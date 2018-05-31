47338252613234f89887e1af0132a54908cbbcd7
william@autosteward.com <william@autosteward.com>
2018-05-31 08:49:19 -0400

    comment out express settings

--- pubnub.js
+++ pubnub.js
@@ -5,19 +5,19 @@ var fs = require ('fs');
 var Twitter = require ('twitter');
 var pubnub = require("pubnub");
 
-// Add express support for port management
-var express = require('express');
-var app     = express();
+// // Add express support for port management
+// var express = require('express');
+// var app     = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+// //For avoidong Heroku $PORT error
+// app.get('/', function(request, response) {
+//     var result = 'App is running'
+//     response.send(result);
+// }).listen(app.get('port'), function() {
+//     console.log('App is running, server is listening on port ', app.get('port'));
+// });
 
 
 
